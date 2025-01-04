a823321f14e94d5a2425546b5eebf540aba12b11
Fabio <fabiote2003@gmail.com>
2025-01-04 03:46:24 -0300

    compress background hero image

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section
       id="inicio"
-      className={`flex flex-col gap-20 items-center h-[calc(100vh-64px)] w-full bg-no-repeat bg-cover bg-center bg-[url('/images/background-image-hero.jpg')] overflow-hidden justify-center`}
+      className={`flex flex-col gap-20 items-center h-[calc(100vh-64px)] w-full bg-no-repeat bg-cover bg-center bg-[url('/images/background-image-hero.webp')] overflow-hidden justify-center`}
     >
       <ReservaForm />
     </section>
